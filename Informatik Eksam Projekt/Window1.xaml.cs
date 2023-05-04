1e64739c8ddfbb46c81e02baf972385b1595d5ae
ThoVig <thoger.hansen@gmail.com>
2023-05-04 17:18:17 +0200

    new window

--- Informatik Eksam Projekt/Window1.xaml.cs
+++ Informatik Eksam Projekt/Window1.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Informatik_Eksam_Projekt
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void newWindow(object sender, RoutedEventArgs e)
-        {
-            Window1 win1 = new Window1();
-            win1.Show();
-            Close();
         }
     }
 }
